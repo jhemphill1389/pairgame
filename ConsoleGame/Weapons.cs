d2fbeb62ed77d2e5f835b6fada2b1a978309af56
jhemphill1389 <ks.jah.1776@gmail.com>
2020-09-29 13:33:27 -0500

    first commit

---
+++ ConsoleGame/Weapons.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ConsoleGame
+{
+    class Weapons
+    {
+
+         
+        static public void Main()
+        {
+
+            // Creating a dictionary 
+            // using Dictionary<TKey,TValue> class 
+            Dictionary<int, string> My_dict =
+                         new Dictionary<int, string>();
+
+            // Adding key/value pairs in the  
+            // Dictionary Using Add() method 
+            My_dict.Add(1, "Fish");
+            My_dict.Add(2, "Frying pan");
+            My_dict.Add(3, "Brick");
+            My_dict.Add(4, "Crowbar");
+            My_dict.Add(5, "Knife");
+            My_dict.Add(6, "Sword");
+            My_dict.Add(7, "Pistol");
+            My_dict.Add(8, "Double-Barelled Shotgun");
+            My_dict.Add(9, "Bomb");
+            My_dict.Add(10, "Shield");
+
+
+
+            for (int i = 1; i < 10; ++i)
+            {
+                int index = random.Next(dictionary.Count);
+
+                //string key = dictionary.Keys.ElementAt(index);
+                
+                KeyValuePair<string, int> pair = Dictionary.ElementAt(index);
+
+                Console.WriteLine("key: " + pair.Key + ", value: " + pair.Value);
+            }
+        }
+    }
+}
+
+   
---
+++ pairgame/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("pairgame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pairgame")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f91940cc-df15-44a1-9880-2674a69563bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
